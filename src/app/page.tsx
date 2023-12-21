d679b57564a79c7c84b230fe370406799ce2d24a
venture.prapanna <venture.prapanna@gmail.com>
2023-12-21 16:11:38 +0545

    Created a Custom Reusable Button | It provies resuable variants, sizes and colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
-import Image from "next/image";
-
+ import Button from "@/components/Button";
+ 
 export default function Home() {
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-5xl font-extrabold">Welcome to Sifaris</h1>
