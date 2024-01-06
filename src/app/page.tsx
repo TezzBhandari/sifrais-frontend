3698a5a0088d92317e980a5ed4f40e01dcf02002
TezzBhandari <venture.tezzbhandari18@gmail.com>
2024-01-06 07:50:15 +0545

    removed some uncessary import and code from app/page.tsx file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
-"use client";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
