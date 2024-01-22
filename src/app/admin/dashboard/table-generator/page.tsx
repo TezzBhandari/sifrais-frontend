42ddea804679d3ca521259045e183a8df87505ca
Tezz Bhandari <venture.tezzbhandari18@gmail.com>
2024-01-22 10:51:55 +0545

    table creation form complete as well as generation part

---
+++ src/app/admin/dashboard/table-generator/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import TableGenerator from './components/TableGenerator'
+import CreateTable from './components/CreateTable'
+
+const page = () => {
+  return (
+    <div className="rounded-xl bg-white p-8">
+     <CreateTable/>
+    </div>
+  )
+}
+
+export default page
