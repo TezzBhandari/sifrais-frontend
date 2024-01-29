afd96f874e2eb4c20b06d26bbf92c35ff4ed1b65
Tezz Bhandari <venture.tezzbhandari18@gmail.com>
2024-01-29 17:19:02 +0545

    create form preview

--- src/app/dashboard/admin/dashboard/system-configuration/sifaris/add/components/FormPreviewModal.tsx
+++ src/app/dashboard/admin/dashboard/system-configuration/sifaris/add/components/FormPreviewModal.tsx
@@ -1,9 +1,193 @@
 import React from 'react'
+import { FormProvider, useForm, useFormContext } from 'react-hook-form';
 
-const FormPreviewModal = () => {
+import type { PreviewModalProps, InputGroup, CreateSifarisForm, Field } from '../types'
+import { Modal } from '@/components/Modal'
+import TextField from '@/components/PreviewInputFields/TextField';
+import NumberField from '@/components/PreviewInputFields/NumberField';
+import EmailField from '@/components/PreviewInputFields/EmailField';
+import DateField from '@/components/PreviewInputFields/DateField';
+
+// renders input field
+export function renderField([name, fieldAttribute]: [string, Field]) {
+  if (fieldAttribute.type === "text") {
     return (
-        <div>FormPreviewModal</div>
-    )
+      <TextField
+        fieldAttribute={fieldAttribute}
+        name={name}
+      />
+    );
+  }
+
+  if (fieldAttribute.type === "number") {
+    return (
+      <NumberField
+        fieldAttribute={fieldAttribute}
+        name={name}
+      />
+    );
+  }
+
+  if (fieldAttribute.type === "email") {
+    return (
+      <EmailField
+        fieldAttribute={fieldAttribute}
+        name={name}
+      />
+    );
+  }
+
+  if (fieldAttribute.type === "date") {
+    return (
+      <DateField
+        fieldAttribute={fieldAttribute}
+        name={name}
+      />
+    );
+  }
+  return <div>Unknown Type</div>;
+}
+
+
+const FormPreviewModal = ({ isOpen, onClose }: PreviewModalProps) => {
+  const sifarisForm = useFormContext<CreateSifarisForm>()
+
+  const formPreviewData = sifarisForm.watch();
+  console.log('form preiew data: ', formPreviewData);
+
+
+  // const formPreviewData: CreateSifarisForm = {
+  //   formName: 'User Form',
+  //   inputGroups:
+  //     [
+  //       {
+  //         groupName: "group-1",
+  //         inputRows: [
+  //           {
+  //             inputfields: [
+  //               {
+  //                 name: "fullname",
+  //                 label: "Full Name",
+  //                 type: "text",
+  //                 id: "fulsdfsdfsdlname",
+  //                 required: true,
+  //               },
+  //               {
+  //                 name: "email",
+  //                 label: "Email",
+  //                 type: "text",
+  //                 id: "emdsfsetrrtwail",
+  //                 required: true,
+  //               },
+  //               {
+  //                 name: "email",
+  //                 label: "Email",
+  //                 type: "text",
+  //                 id: "emadsfsdfweril",
+  //                 required: true,
+  //               },
+  //             ],
+  //           },
+  //         ],
+  //       },
+  //       {
+  //         groupName: "group-2",
+  //         inputRows: [
+  //           {
+  //             inputfields: [
+  //               {
+  //                 name: "fullname",
+  //                 label: "Full Name",
+  //                 type: "text",
+  //                 id: "skflskdf",
+  //                 required: true,
+  //               },
+  //               {
+  //                 name: "email",
+  //                 label: "Email",
+  //                 type: "text",
+  //                 id: "email",
+  //                 required: true,
+  //               },
+  //               {
+  //                 name: "email",
+  //                 label: "Email",
+  //                 type: "text",
+  //                 id: "emsdkfjlskdjfkail",
+  //                 required: true,
+  //               },
+  //             ],
+  //           },
+  //         ],
+  //       },
+  //     ]
+  // }
+
+  const previewModalForm = useForm()
+
+  return (
+    <Modal className="max-w-6xl w-full" isOpen={isOpen} onClose={onClose}>
+      <FormProvider {...previewModalForm}>
+        <form className='relative' onSubmit={previewModalForm.handleSubmit((data) => alert(JSON.stringify(data)))}>
+          <div className="absolute top-2 right-2 cursor-pointer" onClick={(e) => {
+            e.preventDefault();
+            onClose();
+            previewModalForm.reset()
+          }}><span>X</span></div>
+          <div><h1 className="font-bold text-3xl">{formPreviewData.formName}</h1></div>
+          <div className="flex flex-col gap-8">
+            {
+              formPreviewData.inputGroups.map((inputGroup, inputGroupIndex) => {
+
+                // rendering group
+                return <fieldset key={inputGroupIndex} className='border-2 px-2 pt-2 pb-4 rounded-lg'>
+                  <legend className="text-xl font-semibold capitalize">{inputGroup.groupName}</legend>
+                  {/* rendering column */}
+                  {
+                    inputGroup.inputRows.map((inputRow, inputRowIndex) => {
+                      return <div key={inputRowIndex}
+                        style={{
+                          display: "grid",
+                          gridTemplateColumns: `repeat(${inputRow.inputfields.length}, 1fr)`,
+                          gap: "1.5rem",
+                        }}
+                      >
+                        {
+                          Array.isArray(inputRow.inputfields)
+                            ? inputRow.inputfields
+                              .map((field) => {
+                                return [field.name, field] as [string, Field];
+                              }).map(([name, fieldAttribute], fieldIndex) => {
+                                return (
+                                  <div
+                                    key={fieldAttribute.id}
+                                    className="flex items-center gap-4"
+                                  >
+                                    <div className="flex-1">
+                                      {renderField([name, fieldAttribute])}
+                                    </div>
+                                  </div>
+                                );
+                              })
+
+                            // .map(renderField)
+                            // this part is not required here but need for a library build 
+                            : Object.entries(inputRow.inputfields).map(renderField)
+                        }
+                      </div>
+                    })
+                  }
+
+
+                </fieldset>
+              })
+            }
+          </div>
+          <div className="mt-4 flex justify-end items-center"><button className="px-2 py-3 border border-blue-950 rounded-lg">Submit</button></div>
+        </form>
+      </FormProvider>
+    </Modal>
+  )
 }
 
 export default FormPreviewModal
